1761ab166a521c4008390ea96880ccb029fe27df
Ilham Ibnu Purnomo <social.inupurnomo@gmail.com>
2019-08-30 00:15:06 +0700

    init commit

---
+++ web-application/viper.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"net/http"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Set loc config file
+	viper.SetConfigFile("./config/env.json")
+
+	// Show error jika file config tidak ada
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error config file!, &s", err)
+	}
+
+	// Set route ketika web diakses
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %q", html.EscapeString(r.URL.Path))
+	})
+
+	// Run server sesuai port pada file config
+	http.ListenAndServe(":"+viper.GetString("server.port"), nil)
+}
